c32cc6096ee87d0664a397b4ae7461a08092518f
Nehorim <pierrick.chouzenoux@gmail.com>
2014-12-07 19:34:51 +0100

    Initialisation du projet

---
+++ app/scripts/main.js
@@ -0,0 +1,9 @@
+'use strict';
+// console.log('\'Allo \'Allo!');
+
+// $(document).ready(function() {
+// 	$('#headerMenu > li').click(function(){
+// 		$('#headerMenu > li.active').removeClass('active');
+// 		$(this).addClass('active');
+// 	});
+// });
